498de06a60bf246b863ab771237bce3be02275da
Lobanov Pavel <lobanov.ps@gmail.com>
2021-05-21 17:04:56 +0300

    Update version

--- grpc/message_service/cmd/client/main.go
+++ grpc/message_service/cmd/client/main.go
@@ -23,7 +23,7 @@ func AboutToSayIt(ctx context.Context, m p.MessageServiceClient,
 }
 
 func main() {
-	fmt.Println("version 0.10.0")
+	fmt.Println("version 0.15.0")
 	conn, err := grpc.Dial(port, grpc.WithInsecure())
 	if err != nil {
 		fmt.Println("Dial:", err)
